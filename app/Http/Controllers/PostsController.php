e455ce9f22cde36a7516ce86ad0a126ec214fe79
Nikol Paraskova <nicoleparaskova@gmail.com>
2018-05-17 23:48:54 +0300

    Fix mass assignment of posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Category;
+use App\Post;
+use Session;
 
 class PostsController extends Controller
 {
@@ -42,6 +44,19 @@ class PostsController extends Controller
             'category_id' => 'required'
         ]);
       
+        $featured = $request->featured;
+
+        $featured_new_name = time().$featured->getClientOriginalName();
+        $featured->move('uploads/posts', $featured_new_name);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'content'=> $request->content,
+            'featured' => 'uploads/posts/' . $featured_new_name,
+            'category_id' => $request->category_id
+        ]);
+
+        Session::flash('success', 'Post created successfuly');
 
         dd($request->all());
     }
--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+   protected $fillable = [
+       'title', 'content' ,'category_id', 'featured'
+   ];
+   
    public function category()
    {
        $this->belongsTo('App\Category');
